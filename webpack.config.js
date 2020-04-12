233752fd065d0ac6223e80af6931a3517df8491d
Ömer Muhlis Erdoğan <62306742+omer338@users.noreply.github.com>
2020-04-12 23:34:58 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: ["@babel/polyfill",'./src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'bundles'),
+    filename: 'bundle.js'
+  },
+  mode:"production",
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { 
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    port: 3200,
+    index: 'index.html'
+  },
+
+
+ 
+};
